550fd17ea24e3abc1eb86e368fa84ecaafbe0727
Michael A Woodcock <michael@inksquared.com>
2014-02-26 16:01:14 -0500

    Fixed PHP function to render the derby

--- sites/all/themes/whitehousederby/page--node--2.tpl.php
+++ sites/all/themes/whitehousederby/page--node--2.tpl.php
@@ -10,20 +10,19 @@ require("candidates.php");
 
 $i = 0;
 
-$candidateVotes = 20;
-
 foreach($candidates as $candidate => $candidateData) {
 	
+	$query = "SELECT data FROM {webform_submitted_data} WHERE data = '$candidate'";
 	
-	//$voteCount = db_query("SELECT * FROM {webform_submitted_data} WHERE data = 'bush'");
+	$voteCount = db_query($query);
 	
-	//$number_of_rows = $voteCount->rowCount();
+	$number_of_rows = $voteCount->rowCount();
 	
-	print($number_of_rows);
+	//print($number_of_rows);
 
 	print "<div class='lane'>";
 	print "<div class='candidate'><img src='" . $base_path . "sites/all/themes/whitehousederby/images/candidate/" . $candidateData['image'] . ".jpg' /><div class='fullname'>" . $candidateData['fullname'] . "</div></div>";
-	print "<div class='horseArea'><div class='colorbar'></div><img src='" . $base_path . "sites/all/themes/whitehousederby/images/horses/" . $candidateData['horse'] . ".png' data-position=" . $candidateVotes . " /></div>";
+	print "<div class='horseArea'><div class='colorbar'></div><img src='" . $base_path . "sites/all/themes/whitehousederby/images/horses/" . $candidateData['horse'] . ".png' data-position=" . $number_of_rows . " /></div>";
 	print "<div class='finish'><img src='" . $base_path . "sites/all/themes/whitehousederby/images/whitehouse.jpg' /></div></div><div class='spacer'></div>";
 
 	//Limit to 10 showing
